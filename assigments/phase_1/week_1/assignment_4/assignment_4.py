2ce76b351565ed5706d633ab7faecff3d1c14ee4
truongnq <truongnq@reactplus.com>
2026-05-17 21:27:38 +0700

    phase1/ass4

---
+++ assigments/phase_1/week_1/assignment_4/assignment_4.py
@@ -0,0 +1,50 @@
+def one_hot_encoding(sents, max_len=4):
+    vocab = {}
+    order = 0
+    for sent in sents:
+        for word in sent.split():
+            if word not in vocab:
+                vocab[word] = order
+                order += 1
+                
+    print("Từ điển (Vocab):", vocab)
+    vocab_size = len(vocab)
+    
+    padding_vector = [0 for _ in range(vocab_size)]
+    
+    encoded_sents = []
+    
+    for sent in sents:
+        words = sent.split()
+        
+        if len(words) > max_len:
+            words = words[:max_len]
+            
+        sent_vectors = []
+        for word in words:
+            word_idx = vocab[word]
+            vector = [0 for _ in range(vocab_size)]
+            vector[word_idx] = 1
+            sent_vectors.append(vector)
+            
+        while len(sent_vectors) < max_len:
+            # Thêm vector toàn số 0 vào cuối câu
+            sent_vectors.append(padding_vector)
+            
+        encoded_sents.append(sent_vectors)
+        
+    return encoded_sents
+
+sents = [
+    "I love NLP",    
+    "NLP is fun",    
+    "I love NLP and AI very much" 
+]
+
+result = one_hot_encoding(sents, max_len=4)
+
+for idx, sent_matrix in enumerate(result):
+    for vector in sent_matrix:
+        print(vector)
+
+    print("\n")
